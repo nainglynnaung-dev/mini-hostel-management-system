765386aa90a65870fc8f8809c07b7746b1c2d4d0
Naing Lynn Aung <133348339+nainglynnaung-dev@users.noreply.github.com>
2026-02-18 11:44:48 +0630

    userlist add

--- hostel/src/main/java/com/ly/hostel/repository/AuthUserRepository.java
+++ hostel/src/main/java/com/ly/hostel/repository/AuthUserRepository.java
@@ -2,25 +2,19 @@ package com.ly.hostel.repository;
 
 import com.ly.hostel.data.Status;
 import com.ly.hostel.model.AuthUser;
-<<<<<<< Updated upstream
-=======
 
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
->>>>>>> Stashed changes
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface AuthUserRepository extends JpaRepository<AuthUser, Long> {
-<<<<<<< Updated upstream
-=======
 	
 	Optional<AuthUser> findByUsername(String username);
 
     @Query(value = "select * from users as a where a.role=:role and a.status='ACTIVE'",nativeQuery = true)
     Page<AuthUser> findAllByRole(String role, Pageable pageable);
->>>>>>> Stashed changes
 }
