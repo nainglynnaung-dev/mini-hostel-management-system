372f17103759a9e994c85c0d4bcba1e6a6ba20d9
Naing Lynn Aung <133348339+nainglynnaung-dev@users.noreply.github.com>
2026-02-18 10:50:26 +0630

    security authentication

--- hostel/src/main/java/com/ly/hostel/service/impl/AuthUserServiceImpl.java
+++ hostel/src/main/java/com/ly/hostel/service/impl/AuthUserServiceImpl.java
@@ -1,25 +1,45 @@
 package com.ly.hostel.service.impl;
-
-<<<<<<< HEAD
-import com.ly.hostel.service.AuthUserService;
-
-public class AuthUserServiceImpl implements AuthUserService {
-=======
+import com.ly.hostel.data.RoleStatus;
 import com.ly.hostel.dto.auth.RegisterRequest;
 import com.ly.hostel.dto.auth.SignInRequest;
+import com.ly.hostel.model.AuthUser;
+import com.ly.hostel.repository.AuthUserRepository;
 import com.ly.hostel.service.AuthUserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+
+
 @Service
+@RequiredArgsConstructor
 public class AuthUserServiceImpl implements AuthUserService {
+
+    private final AuthUserRepository authUserRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+
     @Override
     public String register(RegisterRequest registerRequest) {
+        AuthUser authUser = new AuthUser();
+        authUser.setEmail(registerRequest.email());
+        authUser.setPassword(passwordEncoder.encode(registerRequest.password()));
+        authUser.setUsername(registerRequest.username());
+        authUser.setRole(RoleStatus.USER);
+        authUserRepository.save(authUser);
         return "";
     }
 
     @Override
-    public String login(SignInRequest signInRequest) {
-        return "";
+    public Authentication login(SignInRequest signInRequest) {
+        var auth=new UsernamePasswordAuthenticationToken(signInRequest.username(), signInRequest.password());
+        Authentication authentication=authenticationManager.authenticate(auth);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        return authentication;
     }
->>>>>>> 4b02302a088beb3c1d5ae4eece0434ffd2d0c9a3
 }
